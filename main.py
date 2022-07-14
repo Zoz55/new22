11fdd812fe87947275b3d01623fff619e3318b5e
Zoz55 <101155960+Zoz55@users.noreply.github.com>
2022-07-14 23:44:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def getPrediction(filename):
     img = np.expand_dims(img, axis=0)  #Get it tready as input to the network       
     
     pred = my_model.predict(img) #Predict                    
-    pred_class = dic[pred[0]]
+    pred_class = dic[[np.argmax(pred)]]
     
     print("Diagnosis is:", pred_class)
-    return pred_class,pred
+    return pred_class
 
